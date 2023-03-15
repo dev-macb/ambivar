7b5f4905621e0e88186f59c7ab2c858d8809cd31
dev-macb <dev.macb@gmail.com>
2023-03-15 14:38:01 -0300

    Alterando nomes das funções

--- src/Ambivar.php
+++ src/Ambivar.php
@@ -3,7 +3,7 @@
 
 
     class Ambivar {
-        public static function Carregar(string $caminho = __DIR__) {
+        public static function carregar_env(string $caminho = __DIR__) {
             if (file_exists($caminho.'/.env')) return false;
 
             $linhas = file($caminho.'/.env');
@@ -15,14 +15,14 @@
         }
 
 
-        public static function Definir_Variavel(string $nome, string $valor) {
+        public static function escrever_env(string $nome, string $valor) {
             $conteudo_env = file_get_contents('.env');
             $new_env_variable = "\n{$nome}={$valor}";
             file_put_contents('.env', $conteudo_env . $new_env_variable, FILE_APPEND);
         }
 
 
-        public static function Apagar_Variavel(string $nome) {
+        public static function apagar_env(string $nome) {
             $conteudo_env = file_get_contents('.env');
             $pattern = "/^{strtoupper($nome)}=.*/m";
             $new_env_contents = preg_replace($pattern, '', $conteudo_env);
