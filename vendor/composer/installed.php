3caf24c0007a640d74207074d66086a68d1befdf
dev-macb <dev.macb@gmail.com>
2023-03-15 14:54:46 -0300

    Ambivar

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dev-macb/ambivar',
+        'pretty_version' => '1.0.2',
+        'version' => '1.0.2.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dev-macb/ambivar' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
