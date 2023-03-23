51a51ba7e554c15a6b6d860519a25a10978d8afe
dev-macb <dev.macb@gmail.com>
2023-03-23 14:19:03 -0300

    Update Ambivar.php

--- src/Ambivar.php
+++ src/Ambivar.php
@@ -3,12 +3,23 @@
 
 
     class Ambivar{
-        public static function carregar($diretorio){
-            if(!file_exists($diretorio . '/.env')) {
+        public static function carregar($diretorio) {
+            if (!file_exists($diretorio . '/.env')) {
                 return false;
             }
 
             $linhas = file($diretorio . '/.env');
+            foreach($linhas as $linha) {
+                putenv(trim($linha));
+            }
+        }
+
+        public static function dotenv() {
+            if (!file_exists(__DIR__ . '/../../../..' . '/.env')) {
+                return false;
+            }
+
+            $linhas = file(__DIR__ . '/../../../..' . '/.env');
             foreach($linhas as $linha) {
                 putenv(trim($linha));
             }
