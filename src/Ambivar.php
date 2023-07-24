3c352383712c21a50cb15b347be34ac5a1b33d43
dev-macb <dev.macb@gmail.com>
2023-07-24 20:26:47 -0300

    versao 1.2.0

--- src/Ambivar.php
+++ src/Ambivar.php
@@ -1,64 +1,112 @@
 <?php
-    namespace DevMacb;
+    namespace MacB;
 
 
     class Ambivar {
-        public static function dotenv() {
-            if (!file_exists(__DIR__ . '/../../../..' . '/.env')) {
-                return false;
-            }
-            $linhas = file(__DIR__ . '/../../../..' . '/.env');
-            foreach($linhas as $linha) {
-                if ($linha != "\n") {
-                    putenv(trim($linha));
-                }
-            }
-            $arquivos = glob(__DIR__ . '/../../../..' . '/*.env');
-            foreach($arquivos as $arquivo) {
-                $linhas = file($arquivo);
-                foreach($linhas as $linha) {
-                    if ($linha != "\n") {
-                        putenv(trim($linha));
+        private static function analisar_env(string $arquivo_env): array {
+            $lista_env = [];
+
+            if (file_exists($arquivo_env)) {
+                $linhas = file($arquivo_env, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+                
+                foreach ($linhas as $linha) {
+                    if (str_contains($linha, '=') && str_starts_with($linha, '#') === false) {
+                        list($chave, $valor) = explode('=', $linha, 2);
+                        $chave = trim($chave);
+                        $valor = trim($valor);
+                        
+                        if (!empty($chave)) {
+                            $lista_env[$chave] = $valor;
+                        }
                     }
                 }
             }
+
+            return $lista_env;
         }
 
 
-        public static function carregar($diretorio, $arquivo='') {
-            if (!file_exists($diretorio . '/' . $arquivo . '.env')) {
-                return false;
-            }
-            $linhas = file($diretorio . '/' . $arquivo . '.env');
-            foreach($linhas as $linha) {
-                if ($linha != "\n") {
-                    putenv(trim($linha));
-                }
+        private static function definir_ambivar(array $lista_env): void {
+            foreach ($lista_env as $chave => $valor) {
+                putenv("$chave=$valor");
+                $_ENV[$chave] = $valor;
+                $_SERVER[$chave] = $valor;
             }
         }
 
 
-        public static function escrever_env($diretorio, $arquivo='', $chave, $valor) {
-            $conteudo_env = file_get_contents($diretorio . '/' . $arquivo . '.env');
-            if (getenv(strtoupper($chave)) !== false) {
-                $padrao = "/^{strtoupper($chave)}=(.*)(\r?\n|$)/m";
-                $nova_env = preg_replace($padrao, "{$chave}={$valor}\n", $conteudo_env);
-                file_put_contents($diretorio . '/' . $arquivo . '.env', $nova_env);
+        public static function dotenv(): void {
+            $caminho_env = __DIR__.'/../../../../.env';
+            $lista_env = self::analisar_env($caminho_env);
+            self::definir_ambivar($lista_env);
+        }
+
+
+        public static function carregar(string $caminho_env = __DIR__.'/../../../../.env'): void {
+            if (file_exists($caminho_env)) {
+                $lista_env = self::analisar_env($caminho_env);
+                self::definir_ambivar($lista_env);
+            }
+        }
+
+
+        public static function obter(string $chave, mixed $valor_padrao = null): mixed {
+            return self::existe($chave) ? getenv($chave) : $valor_padrao;
+        }
+
+
+        public static function existe(string $chave): bool {
+            return getenv(strtoupper($chave)) !== false;
+        }
+
+
+        public static function gravar(string $chave, mixed $valor, string $caminho_env = __DIR__.'/../../../../.env'): bool {
+            if (!file_exists($caminho_env)) return false;
+            
+            $conteudo_env = file_get_contents($caminho_env);
+
+            if (self::existe(strtoupper($chave))) {
+                $padrao = "/^" . preg_quote(strtoupper($chave), '/') . "=(.*)(\r?\n|$)/m";
+                $conteudo_env = preg_replace($padrao, "{$chave}={$valor}\n", $conteudo_env);
             } 
             else {
-                $nova_env = strtoupper($chave) . '=' . $valor . "\n";
-                file_put_contents($diretorio . '/' . $arquivo . '.env', $nova_env, FILE_APPEND);
-            } 
+                $conteudo_env .= strtoupper($chave) . '=' . $valor . "\n";
+            }
+
+            file_put_contents($caminho_env, $conteudo_env);
+            putenv("$chave=$valor");
+            $_ENV[$chave] = $valor;
+            $_SERVER[$chave] = $valor;
+
             return true;
         }
 
 
-        public static function apagar_env($diretorio, $arquivo='', $chave) {
-            $conteudo_env = file_get_contents($diretorio . '/' . $arquivo . '.env');
-            if (getenv(strtoupper($chave)) !== false) {
-                $apagar_env = preg_replace("/^{$chave}=(.*)(\r?\n|$)/m", '', $conteudo_env);
-                file_put_contents($diretorio . '/' . $arquivo . '.env', $apagar_env);
+        public static function remover(string $chave, string $caminho_env = __DIR__.'/../../../../.env'): bool {
+            if (!file_exists($caminho_env)) return false;
+
+            $conteudo_env = file_get_contents($caminho_env);
+
+            if (self::existe($chave)) {
+                $padrao = "/^" . preg_quote(strtoupper($chave), '/') . "=(.*)(\r?\n|$)/m";
+                $apagar_env = preg_replace($padrao, '', $conteudo_env);
+                file_put_contents($caminho_env, $apagar_env);
+
+                unset($_ENV[$chave]);
+                unset($_SERVER[$chave]);
+
                 return true;
+            }
+
+            return false;
+        }
+
+
+        public static function carregar_pasta(string $diretorio): void {
+            $arquivos = glob(rtrim($diretorio, '/') . '/*.env');
+            
+            foreach ($arquivos as $arquivo) {
+                self::carregar($arquivo);
             }
         }
     }
